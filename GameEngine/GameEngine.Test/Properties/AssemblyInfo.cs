71c940d757aecbe0316709b1f1732b7e90b82631
abarnard <abarnard@singular.co.za>
2018-07-02 21:02:17 +0200

    Create

---
+++ GameEngine/GameEngine.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameEngine.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameEngine.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c0e79ed-e5ea-48b5-8d90-a9eb8462b7e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GameEngine/GameEngine/PlayerCharacter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace GameEngine
+{
+	public class PlayerCharacter : INotifyPropertyChanged
+	{
+		private int _health = 100;
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string FullName => $"{FirstName} {LastName}";
+
+		public string NickName { get; set; }
+
+		public int Health
+		{
+			get => _health;
+			set
+			{
+				_health = value;
+				//OnPropertyChanged();
+			}
+		}
+
+		public bool IsNoob { get; set; }
+		public List<string> Weapons { get; set; }
+
+		public event EventHandler<EventArgs> PlayerSlept;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public PlayerCharacter()
+		{
+			FirstName = GenerateRandomFirstName();
+			IsNoob = true;
+
+			CreateStartingWeapons();
+		}
+
+		private void CreateStartingWeapons()
+		{
+			
+		}
+		public void Sleep()
+		{
+			var healthIncrease = CalculateHealthIncrease();
+
+			Health += healthIncrease;
+
+			OnPlayerSlept(EventArgs.Empty);
+
+		}
+
+		private int CalculateHealthIncrease()
+		{
+			var rnd = new Random();
+
+			return rnd.Next(1, 101);
+		}
+
+		protected virtual void OnPlayerSlept(EventArgs e)
+		{
+			PlayerSlept?.Invoke(this, e);
+		}
+
+		public void TakeDamage(int damage)
+		{
+			Health = Math.Max(1, Health -= damage);
+		}
+
+		private string GenerateRandomFirstName()
+		{
+			var possibleRandomSTartingNames = new[]
+			{
+				"Danieth",
+				"Derick",
+				"André"
+			};
+
+			return "Random Name";
+		}		
+	}
+}
